35c20e3145ae869dda755a0f43e6de8d7ec545b5
Dushyant Yadav <dushyant9309@gmail.com>
2019-08-20 18:38:31 +0530

    Add basic front-end authentication

--- src/utils/AuthProvider/AuthProvider.js
+++ src/utils/AuthProvider/AuthProvider.js
@@ -1,32 +1,49 @@
 import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'react-admin';
 
-export default (type, params) => {
+const API_URL = 'http://172.17.65.149:8000/boco_portal'
+
+export default async (type, params) => {
     // called when the user attempts to log in
     if (type === AUTH_LOGIN) {
-        const { username } = params;
-        localStorage.setItem('username', username);
-        // accept all username/password combinations
+        const { username, password } = params;
+        const request = new Request(`${API_URL}/sign_in`, {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+        })
+        await fetch(request)
+            .then((response) => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText)
+                }
+                return response.json();
+            })
+            .then(({ JWT }) => {
+                localStorage.setItem('token', JWT);
+                localStorage.setItem('username', username);
+            })
         return Promise.resolve();
-    }
-    // called when the user clicks on the logout button
-    if (type === AUTH_LOGOUT) {
+
+    } else if (type === AUTH_LOGOUT) {
         localStorage.removeItem('username');
+        localStorage.removeItem('token')
         return Promise.resolve();
-    }
-    // called when the API returns an error
-    if (type === AUTH_ERROR) {
+
+    } else if (type === AUTH_ERROR) {
         const { status } = params;
         if (status === 401 || status === 403) {
             localStorage.removeItem('username');
             return Promise.reject();
         }
         return Promise.resolve();
-    }
-    // called when the user navigates to a new location
-    if (type === AUTH_CHECK) {
-        return localStorage.getItem('username')
+
+    } else if (type === AUTH_CHECK) {
+        return localStorage.getItem('token')
             ? Promise.resolve()
-            : Promise.reject();
+            : Promise.reject('You were logged out');
+
+    } else {
+        return Promise.reject('Unknown method');
     }
-    return Promise.reject('Unknown method');
+
 };
