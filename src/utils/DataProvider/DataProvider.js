f2d18f5a31bc2997a37f44e5e0affefca0771333
Dushyant Yadav <dushyant9309@gmail.com>
2019-08-20 19:47:00 +0530

    Bugfix

--- src/utils/DataProvider/DataProvider.js
+++ src/utils/DataProvider/DataProvider.js
@@ -14,6 +14,9 @@ import { stringify } from 'query-string'
 const API_URL = 'http://172.17.65.149:8000/boco_portal'
 
 const convertDataProviderRequestToHTTP = (type, resource, params) => {
+    const token = localStorage.getItem('token') || '';
+    const auth_header = `JWT ${token}`;
+
     switch (type) {
         case GET_LIST: {
             const { page, perPage } = params.pagination;
@@ -23,17 +26,41 @@ const convertDataProviderRequestToHTTP = (type, resource, params) => {
                 range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
                 filter: JSON.stringify(params.filter),
             };
-            return {url: `${API_URL}/${resource}?${stringify(query)}`};
+            return {
+                url: `${API_URL}/${resource}?${stringify(query)}`,
+                options: {
+                    method: 'GET',
+                    headers: new Headers({
+                        Authorization: auth_header
+                    })
+                }
+            };
         }
 
         case GET_ONE: 
-            return { url: `${API_URL}/${resource}/${params.id}`}
+            return { 
+                url: `${API_URL}/${resource}/${params.id}`,
+                options: {
+                    method: 'GET',
+                    headers: new Headers({
+                        Authorization: auth_header
+                    })
+                }
+            }
             
         case GET_MANY: {
             const query = {
                 filter: JSON.stringify({ id: params.ids }),
             };
-            return { url: `${API_URL}/${resource}?${stringify(query)}` };
+            return {
+                url: `${API_URL}/${resource}?${stringify(query)}`,
+                options: {
+                    method: 'GET',
+                    headers: new Headers({
+                        Authorization: auth_header
+                    })
+                }
+            };
         }
 
         case GET_MANY_REFERENCE: {
@@ -44,25 +71,50 @@ const convertDataProviderRequestToHTTP = (type, resource, params) => {
                 range: JSON.stringify([(page - 1) * perPage, (page * perPage) - 1]),
                 filter: JSON.stringify({ ...params.filter, [params.target]: params.id }),
             };
-            return { url: `${API_URL}/${resource}?${stringify(query)}` };
+            return {
+                url: `${API_URL}/${resource}?${stringify(query)}`,
+                options: {
+                    method: 'GET',
+                    headers: new Headers({
+                        Authorization: auth_header
+                    })
+                }
+            };
         }
 
         case UPDATE:
             return {
                 url: `${API_URL}/${resource}/${params.id}`,
-                options: { method: 'POST', body: JSON.stringify(params.data) },
+                options: {
+                    method: 'POST', 
+                    body: JSON.stringify(params.data),
+                    headers: new Headers({
+                        Authorization: auth_header
+                    })
+                },
             };
 
         case CREATE:
             return {
                 url: `${API_URL}/${resource}`,
-                options: { method: 'POST', body: JSON.stringify(params.data) },
+                options: {
+                    method: 'POST', 
+                    body: JSON.stringify(params.data),
+                    headers: new Headers({
+                        Authorization: auth_header
+                    })
+                },
             };
 
         case DELETE:
             return {
                 url: `${API_URL}/${resource}/${params.id}`,
-                options: { method: 'DELETE' },
+                options: {
+                    method: 'DELETE',
+                    headers: new Headers({
+                        Authorization: auth_header
+                    })
+                },
             };
 
         default:
@@ -91,6 +143,7 @@ const convertHTTPResponseToDataProvider = (response, type, resource, params) => 
 export default (type, resource, params) => {
     const { fetchJson } = fetchUtils;
     const { url, options } = convertDataProviderRequestToHTTP(type, resource, params);
+    const token = localStorage.getItem('token') || '';
     return fetchJson(url, options)
         .then(response => convertHTTPResponseToDataProvider(response, type, resource, params))
 }
